2e00bbc51149554b6a3de7878ba55d48d5384c6c
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-07-11 22:17:01 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,39 @@
+
+public class Solution {
+
+    public boolean evaluateTree(TreeNode root) {
+        if (root.val == 0) {
+            return false;
+        }
+        if (root.val == 1) {
+            return true;
+        }
+        
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
+        return root.val == 2 ? (left || right) : (left && right);
+    }
+}
+
+/*
+Class TreeNode is in-built in the solution file on leetcode.com. 
+When running the code on the website, do not include this class.
+ */
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
